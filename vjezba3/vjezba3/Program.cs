bda328c380ec0ee790660bb0de7240023380a6c6
Jakov764 <55740028+Jakov764@users.noreply.github.com>
2020-11-17 11:02:55 +0100

    Add files via upload

---
+++ vjezba3/vjezba3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = new string[3];
+            Console.WriteLine("Upisi 1. string: ");
+            a[0] = Console.ReadLine();
+
+            Console.WriteLine("Upisi 2. string: ");
+            a[1] = Console.ReadLine();
+
+            Console.WriteLine("Upisi 3. string: ");
+            a[2] = Console.ReadLine();
+
+            Array.Sort(a);
+            Array.Reverse(a);
+
+            Console.WriteLine("Raznovrsni stringovi : ");
+            for (int i= 0; i<3; i++)
+            {
+                Console.WriteLine(a[1]);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
